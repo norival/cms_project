d17736881a9fa8fd855790de3c3e1604fc15d6a9
Xavier Laviron <xavier.laviron@gmx.fr>
2019-12-12 09:49:32 +0100

    adds User fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+     private $passwordEncoder;
+
+     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+     {
+         $this->passwordEncoder = $passwordEncoder;
+     }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('xavier@xavier.fr');
+        $user->setFirstname('Xavier');
+        $user->setLastname('Norival');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'password'
+        ));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
